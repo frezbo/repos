e233ff59ca1692a8eae66aa529009827007261fd
Noel Georgi <git@frezbo.com>
2020-09-08 16:53:42 +0530

    Fix: lint errors

--- tests/repos.ts
+++ tests/repos.ts
@@ -1,30 +1,30 @@
 import * as pulumi from '@pulumi/pulumi'
 
 pulumi.runtime.setMocks({
-    newResource: function (type: string, name: string, inputs: any): { id: string, state: any } {
-        return {
-            id: inputs.name + "_id",
-            state: inputs,
-        }
-    },
-    call: function (token: string, args: any, provider?: string) {
-        return args;
+  newResource (type: string, name: string, inputs: any): { id: string, state: any } {
+    return {
+      id: inputs.name + '_id',
+      state: inputs,
     }
+  },
+  call (token: string, args: any, provider?: string) {
+    return args;
+  }
 })
 
 import * as repos from '../index'
 
-describe('Repositories', function () {
-    const repositories = repos.repositories
-        repositories.map(repo => {
-            pulumi.all([repo.name, repo.visibility]).apply(([name, visibility]) => {
-                it(`${name} must be public`, function (done) {
-                if (visibility == 'public') {
-                    done()
-                } else {
-                    done(new Error(`visibilty should be public for ${name}`))
-                }
-            })
-        })
+describe('Repositories', () => {
+  const repositories = repos.repositories
+    repositories.map(repo => {
+      pulumi.all([repo.name, repo.visibility]).apply(([name, visibility]) => {
+        it(`${name} must be public`, (done) => {
+        if (visibility === 'public') {
+            done()
+        } else {
+            done(new Error(`visibilty should be public for ${name}`))
+        }
+      })
     })
+  })
 })
