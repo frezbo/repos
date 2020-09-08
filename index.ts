d3eef83549abe8a1e45559ac20ab881292458323
Noel Georgi <git@frezbo.com>
2020-09-08 16:33:25 +0530

    feature: add tests

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as github from '@pulumi/github'
 
-const provider = new github.Provider('github', { token: process.env.GITHUB_TOKEN, owner: 'frezbo' })
+const ghProvider = new github.Provider('github', { token: process.env.GITHUB_TOKEN, owner: 'frezbo' })
 
 const repos = [
   'resume',
@@ -14,7 +14,7 @@ repos.map(repo => {
   repositories.push(new github.Repository(repo, {
     visibility: 'public',
     name: repo
-  }, { provider: provider.getProvider('github') }))
+  }, { provider: ghProvider, import: repo }))
 })
 
 export { repositories }
