28f6f4e8b387c9f46e0d244de870ee36549ac598
Noel Georgi <git@frezbo.com>
2020-09-10 12:49:07 +0530

    Update to latest provider to import

--- index.ts
+++ index.ts
@@ -3,18 +3,32 @@ import * as github from '@pulumi/github'
 const ghProvider = new github.Provider('github', { token: process.env.GITHUB_TOKEN, owner: 'frezbo' })
 
 const repos = [
-  'resume',
-  'repos',
-  'rpminfo'
+  { name: 'resume', description: 'Repository to hold personal Resume' },
+  { name: 'repos', description: 'Manage personal repositories' },
+  { name: 'rpminfo', description: 'Retrieve RPM packages list from yum repo' }
 ]
 
 const repositories: github.Repository[] = []
 
 repos.map(repo => {
-  repositories.push(new github.Repository(repo, {
+  repositories.push(new github.Repository(repo.name, {
     visibility: 'public',
-    name: repo
-  }, { provider: ghProvider, import: repo }))
+    name: repo.name,
+    allowMergeCommit: true,
+    allowRebaseMerge: true,
+    allowSquashMerge: true,
+    archived: false,
+    autoInit: false,
+    defaultBranch: 'main',
+    deleteBranchOnMerge: true,
+    description: repo.description,
+    hasDownloads: true,
+    hasIssues: true,
+    hasProjects: true,
+    hasWiki: true,
+
+    isTemplate: false
+  }, { provider: ghProvider }))
 })
 
 export { repositories }
